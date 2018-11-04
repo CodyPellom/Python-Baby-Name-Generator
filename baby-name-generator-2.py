780549f538bf6837ef84d02e724592ac844a40a9
CodyPellom <paulpellom@gmail.com>
2018-11-04 01:37:07 -0400

    df

--- baby-name-generator-2.py
+++ baby-name-generator-2.py
@@ -1,15 +1,54 @@
 import random, string
-def generator():
-    letter1 = random.choice(string.ascii_lowercase)
-    letter2 = random.choice(string.ascii_lowercase)
-    letter3 = random.choice(string.ascii_lowercase)
-    letter4 = random.choice(string.ascii_lowercase)
-    letter5 = random.choice(string.ascii_lowercase)
-    name = letter1 + letter2 + letter3 + letter4 + letter5
-    return(name)
-print('your baby name is: ')
-print(generator())
 
+letter_input_1 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for any other letter: ')
+letter_input_2 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for any other letter: ')
+letter_input_3 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for any other letter: ')
+letter_input_4 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for any other letter: ')
+letter_input_5 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for any other letter: ')
 
-letter_input_1 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for anything: ')
-letter_input_2 = input('choose a letter ... "v" for vowels, "c" for consonants, "l" for anything: ')
+vowels = "aeiouy"
+consonants = "bcdfghjklmnpqrstuvwxz"
+letter = string.ascii_lowercase
+
+def generator():  
+    if letter_input_1 == "v":
+        return random.choice(vowels)
+    elif letter_input_1 == "c":
+        return random.choice(consonants)
+    elif letter_input_1 == "l":
+        return random.choice(letter)
+    else:
+        return letter_input_1
+    if letter_input_2 == "v":
+        return random.choice(vowels)
+    elif letter_input_2 == "c":
+        return random.choice(consonants)
+    elif letter_input_2 == "l":
+         return random.choice(letter)
+    else:
+        return letter_input_2
+    if letter_input_3 == "v":
+        return random.choice(vowels)
+    elif letter_input_3 == "c":
+        return random.choice(consonants)
+    elif letter_input_3 == "l":
+        return random.choice(letter)
+    else:
+        return letter_input_3
+    if letter_input_4 == "v":
+        return random.choice(vowels)
+    elif letter_input_4 == "c":
+        return random.choice(consonants)
+    elif letter_input_4 == "l":
+        return random.choice(letter)
+    else:
+        return letter_input_4
+    if letter_input_5 == "v":
+        return random.choice(vowels)
+    elif letter_input_5 == "c":
+        return random.choice(consonants)
+    elif letter_input_5 == "l":
+        return random.choice(letter)
+    else:
+        return letter_input_5
+        print(generator())
